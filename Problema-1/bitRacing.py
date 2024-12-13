d73821a168d99ba5f0e0eff21d36b63aec3a2e87
Fernanda2Olan <al20760686@ite.edu.mx>
2024-12-12 16:14:38 -0800

    no seq

--- Problema-1/bitRacing.py
+++ Problema-1/bitRacing.py
@@ -1,4 +1,10 @@
 import math
  
-def sumar_digito(numero): 
-    while >= 
+def suma_de_dos_digitos(num): 
+    while num >= 10:
+        num = sum(int(d) for d in str(num))
+    return num
+
+def carrera(num):
+    digitos = [int(d) for d in str(num)]
+    
